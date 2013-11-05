e6ac38c25dad178c412ac675eae0600a248cdd0a
Akshay Patel <akshayrpatel@gmail.com>
2013-11-05 11:26:27 -0800

    Simplify

--- conf/nav.js
+++ conf/nav.js
@@ -16,27 +16,6 @@ module.exports = function () {
             'key': 'archives',
             'description': 'Archives',
             'url': '/archives/'
-        },
-        {
-            'key': 'code',
-            'description': 'Code',
-            'url': 'http://akshayp.github.io/',
-            'external': true
-        },
-        {
-            'key': 'geektool-scripts',
-            'description': 'Geektool Scripts',
-            'url': '/geektool-scripts/'
-        },
-        {
-            'key': 'perl',
-            'description': 'Perl',
-            'url': '/perl/'
-        },
-        {
-            'key': 'portfolio',
-            'description': 'Portfolio',
-            'url': '/portfolio/'
         }
     ];
 };

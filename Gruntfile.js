9582a8975831deb3a2545eb02f15a2febb854fc7
sbertal <seth@bertalotto.net>
2013-11-06 11:07:30 -0800

    ocd

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-handlebars');
 
     // setup tasks
     grunt.registerTask('assets', ['clean', 'cssproc', 'concat', 'uglify']);
